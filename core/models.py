3e8dae23aaeaeeb308f7cde667668084749f8a0d
karinavolodina <volodinakarina27@yandex.ru>
2023-07-05 23:46:14 +0300

    change1

--- core/models.py
+++ core/models.py
@@ -1,33 +1,3 @@
-# from django.contrib.auth.models import AbstractUser
-# from django.db import models
-#
-# # Create your models here.
-# class User(AbstractUser):
-#     REQUIRED_FIELDS = []
-#
-# #
-# # class Ad(models.Model):
-# #     title = models.CharField(max_length=100)
-# #     price = models.PositiveIntegerField()
-# #     description = models.TextField(max_length=2000)
-# #     author = models.ForeignKey('users.User', on_delete=models.CASCADE)
-# #     created_at = models.DateTimeField(auto_now_add=True)
-# #     image = models.ImageField(upload_to='images/ads/', null=True, blank=True)
-# #     is_active = models.BooleanField(default=True)
-# #
-# #     class Meta:
-# #         ordering = ['-created_at']
-# #
-# #
-# # class Comment(models.Model):
-# #     text = models.TextField(max_length=500)
-# #     author = models.ForeignKey('users.User', on_delete=models.CASCADE)
-# #     ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
-# #     created_at = models.DateTimeField(auto_now_add=True)
-# #     is_active = models.BooleanField(default=True)
-# #
-# #     class Meta:
-# #         ordering = ['created_at']
 from django.conf import settings
 from django.db import models
 
--- todolist/urls.py
+++ todolist/urls.py
@@ -1,22 +1,42 @@
-"""
-URL configuration for todolist project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# """
+# URL configuration for todolist project.
+#
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/4.2/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
+# from django.contrib import admin
+# from django.urls import path
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("api/admin/", admin.site.urls),
+    path("api/redoc-tasks/", include("redoc.urls")),
+
+    path('', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
+
+    path('refresh/', TokenRefreshView.as_view()),
+    path('api/', include('users.urls')),
+    path('api/', include('core.urls'))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
